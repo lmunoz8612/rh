4fe65fa58457d8093f74aaa0db019ae3fe72bc54
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2025-06-19 00:44:52 -0600

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 window.env = {
-    REACT_APP_API_URL: 'https://54.183.233.131/rh-api',
+    REACT_APP_API_URL: 'https://54.183.233.131/rh-api/public/index.php',
     REACT_APP_ENCRIPT_PASSWORD_KEY: 'oc7yqqXqiaNOqFAMhRUbPo3dGqDI8wIB',
 };
