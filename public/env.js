0a7a95e8dac2a7051a58b4e22df2a68cbc233391
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2026-02-17 17:33:25 -0600

    Update API URL in env.js

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 window.env = {
-    REACT_APP_API_URL: 'https://54.183.233.131/rh-api',
+    REACT_APP_API_URL: 'https://rh-api-c4d8akbha9d8h9bx.mexicocentral-01.azurewebsites.net/public/index.php',
     REACT_APP_ENCRIPT_PASSWORD_KEY: 'oc7yqqXqiaNOqFAMhRUbPo3dGqDI8wIB',
 };
