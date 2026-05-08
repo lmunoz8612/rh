0d3c7ecfe2caa96212246757cc427bc190f969f5
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2026-05-08 12:05:11 -0600

    Fix typo in environment variable for password encryption

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
 
     const encryptedPassword = (password) => {
         const iv = CryptoJS.lib.WordArray.random(16);
-        const secretKey = process.env.REACT_APP_ENCRIPT_PASSWORD_KEY || window.env.REACT_APP_ENCRIPT_PASSWORD_KEY;
+        const secretKey = process.env.REACT_APP_ENCRYPT_PASSWORD_KEY;
         const encrypted = CryptoJS.AES.encrypt(password, CryptoJS.enc.Utf8.parse(secretKey), {
             iv: iv,
             mode: CryptoJS.mode.CBC,
